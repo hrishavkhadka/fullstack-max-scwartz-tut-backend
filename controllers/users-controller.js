2f04dba5be913b46eef1ef67667707dfa07e16b3
hrishav <notrisavkhadka@gmail.com>
2025-10-30 13:52:01 +0545

    to save

---
+++ controllers/users-controller.js
@@ -0,0 +1,56 @@
+const { v4: uuid } = require("uuid");
+const { validationResult } = require("express-validator");
+
+const HttpError = require("../models/http-error");
+
+const DUMMY_USERS = [
+  {
+    id: "u1",
+    name: "hrishav khadka",
+    email: "test@test.com",
+    password: "testers",
+  },
+];
+
+const getUsers = (req, res, next) => {
+  res.status(200).json({ users: DUMMY_USERS });
+};
+
+const signup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invalid Inputs Passed, Please check your data.");
+  }
+  const { name, email, password } = req.body;
+
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+  if (hasUser) {
+    throw new HttpError("Could not create user, Email already in use.", 422);
+  }
+
+  const createdUser = {
+    id: uuid(),
+    name,
+    email,
+    password,
+  };
+
+  DUMMY_USERS.push(createdUser);
+  res.status(201).json({ user: createdUser });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError("Could not identify the user, wrong credentials", 401);
+  }
+
+  res.json({ message: "logged in" });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
